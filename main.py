be982a0a1ddf520cc08ae55c0c5be440ca8c9fbf
Aadip111 <>
2021-11-29 02:00:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+sequence = [1,6,45,987]
+
+
+#sequences
+print(sequence[1])
+print(sequence[-1])  #prints last element in sequence
+print(sequence[0:2]) #returns objects 0 and 1, not 2 or the last object specified
+
+#lists
+numbers = [2,4,6,8]
+print(numbers[0])
