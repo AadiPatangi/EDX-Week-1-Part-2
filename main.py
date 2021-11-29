078c54b306684ff396e19caf1018454aa1d207b1
Aadi Patangi <87159377+AadiPatangi@users.noreply.github.com>
2021-11-29 02:22:12 +0000

    added notes

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ print(sequence[0:2]) #returns objects 0 and 1, not 2 or the last object specifie
 #lists
 numbers = [2,4,6,8]
 print(numbers[0])
+print(numbers[-1])
+numbers.append(10) #adds 10 to the end of the list
+print(numbers)
+x = [12,14,16] 
+numbers = numbers + x #adding list x to numbers
+print(numbers)
+print(type(numbers))
+numbers.reverse  # reversing the list
+print(numbers)
+locations = ["New York","Albany","Brisbane","San Francisco"]
+locations.sort() #sorts the locations list in alphabetical order
+print(locations)
+print(len(locations)) #returns amount of objects in the list
+
+#tuples
